86ad0f9c77fca2300d630a26c1f775b4f48ad596
andreadelogu42-creator <andreadelogu42@gmail.com>
2026-05-29 20:37:06 +0200

    Add files via upload

---
+++ IMPARARE PYTHON/01_Basi_Variabili/Tipi_di_dati.py
@@ -0,0 +1,67 @@
+#-----------------------
+# ESEMPI DI TIPI DI DATI 
+#------------------------
+
+#------------------------------
+# PRIMO VERIFICHIAMO UN INTERO 
+#------------------------------
+"""
+numero_intero = 45 #dichiaramo la variabile con il valore assegnato
+print(type(numero_intero)) # mandiamo a schermo il tipo della variabile
+"""
+
+#-------------------------------------
+# SECONDO VERIFICHAIMO IL TIPO FLOAT 
+#--------------------------------------
+"""
+numero_decimale = 12.5
+print(type(numero_decimale))
+"""
+
+#-------------------------------------
+# VERIFICHAIMO IL TIPO STRINGA
+#--------------------------------------
+"""
+testo = "Ciao, mi chiamo Andrea"
+print(type(testo))
+"""
+
+#-------------------------------------
+#  VERIFICHAIMO IL TIPO BOOLEANO
+#--------------------------------------
+
+"""
+valore_booleano = True
+print(type(valore_booleano))
+"""
+
+#-------------------------------------
+#  VERIFICHAIMO IL TIPO LISTA
+#--------------------------------------
+"""
+numeri = [3, 6, 7, 9, 10]
+print(type(numeri))
+"""
+
+#-------------------------------------------
+#  VERIFICHAIMO IL TIPO LISTA MISTA
+#-------------------------------------------
+"""
+misto = [3, 6.14, "testo", True]
+print(type(misto))
+"""
+
+#-------------------------------------
+# VERIFICHAIMO IL TIPO TUPLA
+#--------------------------------------
+"""
+giorni_settimana = ("Lunedì", "Martedì", "Mercoledì" "Giovedì", "Venerdì", "Sabato", "Domenica")
+print(type(giorni_settimana))
+"""
+
+#-------------------------------------
+#  VERIFICHAIMO IL TIPO DICTIONARY
+#--------------------------------------
+
+informazioni_personali = {"Nome": "Andrea", "Cognome": "Delogu", "eta": 27, "Città": "Sassari"}
+print(type(informazioni_personali))

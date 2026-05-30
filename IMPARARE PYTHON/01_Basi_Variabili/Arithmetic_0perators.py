b4f46f80011bd177d4c7b752571df51e3f4f2180
andreadelogu42-creator <andreadelogu42@gmail.com>
2026-05-30 09:52:25 +0200

    Add files via upload

---
+++ IMPARARE PYTHON/01_Basi_Variabili/Arithmetic_0perators.py
@@ -0,0 +1,63 @@
+#-----------------------
+#  EXERCISES ARITHMETIC OPERATORS 
+#-----------------------
+
+#--------------------------------------------
+
+# The first arithmetic operator is addition 
+
+#---------------------------------------------
+"""
+result = 10 + 3 
+print(result)
+"""
+
+#--------------------------------------------
+
+# The second arithmetic operator is subraction
+
+#---------------------------------------------
+"""
+result = 10 - 3 
+print(result)
+""" 
+
+#--------------------------------------------
+
+# The third arithmetic operator is multiplication
+
+#---------------------------------------------
+"""
+result = 10 * 4
+print(result)
+"""
+#--------------------------------------------
+
+# The first arithmetic operator is division
+
+#---------------------------------------------
+"""
+result = 10 / 2 # da resto 3.333333333333333 
+print(result)
+#--------------------------------------------
+"""
+#---------------------------------------------#
+
+# Con il doppio // 
+"""
+result = 10 // 3 # Da solo la parte intera 
+print(result)
+#--------------------------------------------
+"""
+# Con il modulo % 
+"""
+result = 10 % 3 # resto della divisone: 3 *= 9 + 1 = 10 
+print(result)
+"""
+#----------------------------------------------------------
+
+# Esercizio 
+
+eta = 32 
+eta += 1 # Incrementa di 1
+print(eta)

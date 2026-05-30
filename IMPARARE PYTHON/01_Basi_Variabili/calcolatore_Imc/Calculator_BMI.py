52d25bd019836c23b467e23e5c83b7176a9d717e
andreadelogu42-creator <andreadelogu42@gmail.com>
2026-05-30 11:21:39 +0200

    Enhance BMI calculator with age input and name formatting
    Added age input and improved name formatting.

--- IMPARARE PYTHON/01_Basi_Variabili/calcolatore_Imc/Calculator_BMI.py
+++ IMPARARE PYTHON/01_Basi_Variabili/calcolatore_Imc/Calculator_BMI.py
@@ -3,9 +3,23 @@
 # CALCULATOR BMI (Body Mass Index). 
 
 #-----------------------------------
-name = input("Name: ")
+name = input("Name: ").strip().tiitle()
 weight = int(input("Weight in (kg): "))
 height = float(input("Height(in m, es 1.75): "))
-
+eta = int(input(Età: ")
+                
+# Calculation of BMI
 bmi = weight / (height ** 2)
-print(bmi)
+
+# Let's check the body mass index values
+
+if bmi < 18.5:
+    print("State: Underweight")
+elif bmi < 25.0: 
+    print("State: Normalweight")
+elif bmi < 30.0: 
+    print("State: Overeight")
+else: 
+    print("Obese")
+
+print(f"{name} your BMI is: {bmi:.2f}")
